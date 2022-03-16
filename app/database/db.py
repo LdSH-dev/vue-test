e481556aed1a7500ed887812d512187511f25528
Leonardo da Silva Hemming <82173546+LdSH-dev@users.noreply.github.com>
2022-03-16 15:02:20 -0300

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -6,14 +6,14 @@ import sqlite3
 # caso o banco não exista ele será criado
 conn = sqlite3.connect("app.db")
 
-# def add_filme(name,url):
-#     conn.execute("insert into filme (name, url) values ("name.",".url.")")
-#     conn.commit()
+def add_filme(name,url):
+    conn.execute("insert into filme (name, url) values ("name.",".url.")")
+    conn.commit()
 
-# def remove_filme(id):
-#     """ remove o filme da tabela """
-#     conn.execute("delete from tarefa where cd_tarefa = ?", (id, ))
-#     conn.commit()
+def remove_filme(id):
+    """ remove o filme da tabela """
+    conn.execute("delete from tarefa where cd_tarefa = ?", (id, ))
+    conn.commit()
 
 def get_filmes(): # retorna um cursor
     """ retorna a lista de filmes """
